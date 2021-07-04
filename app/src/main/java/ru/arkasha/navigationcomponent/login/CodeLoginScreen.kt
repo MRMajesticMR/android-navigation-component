d043042812d98250d2fd18f967be3442f7d5e188
arkadiy.zakharov <arkadiy,zakharov@doconcall.ru>
2021-07-04 11:08:13 +0300

    Try out popUpTo

--- app/src/main/java/ru/arkasha/navigationcomponent/login/CodeLoginScreen.kt
+++ app/src/main/java/ru/arkasha/navigationcomponent/login/CodeLoginScreen.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import ru.arkasha.navigationcomponent.MainScreenDirections
 import ru.arkasha.navigationcomponent.R
 
 class CodeLoginScreen : Fragment(R.layout.s_login_code) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<View>(R.id.bShowGlobal).setOnClickListener {
-            findNavController().navigate(MainScreenDirections.actionGlobalGlobalScreen())
+//        view.findViewById<View>(R.id.bShowGlobal).setOnClickListener {
+//            findNavController().navigate(MainScreenDirections.actionGlobalGlobalScreen())
+//        }
+
+        view.findViewById<View>(R.id.bSendCode).setOnClickListener {
+            findNavController().navigate(CodeLoginScreenDirections.actionCodeLoginScreenToEmailLoginScreen())
         }
     }
 
